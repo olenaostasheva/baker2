f95d22b2fc61169ccb0e2ad4ae02e6015ad5612e
David Kane <dkane@hutchinhill.local>
2016-04-21 15:21:00 -0400

    comments

--- R/gather_monthly.R
+++ R/gather_monthly.R
@@ -14,10 +14,13 @@
 
 gather_monthly <- function(x){
   
+  
+  
+  
+  
   ## We really only need this data for the last day of the month, so, we can 
   ## filter out the rest as here. Keep in mind that this is the last trade day
-  ## of the month, not always the last day. Maybe just merge this in with
-  ## gather_daily()?
+  ## of the month, not always the last day. 
   
   monthly <- x %>% group_by(month) %>% 
     filter(min_rank(desc(date)) == 1 & ! is.na(sd.252.0.d)) 
